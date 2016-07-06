72875ff70afa160ed3618474814a83146fa60ecc
Durran Jordan <durran@gmail.com>
2016-07-06 16:28:45 +0200

    Adding sample component and actions

---
+++ index.js
@@ -0,0 +1,2 @@
+module.exports.Actions = require('./lib/actions');
+module.exports.CurrentOp = require('./lib/components/current-op');
---
+++ src/actions.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const Reflux = require('reflux');
+
+/**
+ * The actions used by the server stats components.
+ */
+const Actions = Reflux.createActions([
+  'currentOp'
+]);
+
+module.exports = Actions;
