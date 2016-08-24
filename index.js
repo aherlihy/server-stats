6ee60270e86cda17ef56ef297058f8a8c3f1a9fe
aherlihy <anna.herlihy@10gen.com>
2016-08-24 14:41:13 +0200

    added components/stores for 3 other graphs

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 module.exports.Actions = require('./lib/actions');
 module.exports.CurrentOpComponent = require('./lib/components/current-op-component');
-module.exports.OpCountersComponent = require('./lib/components/opcounters-component');
 module.exports.TopComponent = require('./lib/components/top-component');
+
+module.exports.NetworkComponent = require('./lib/components/network-component');
+module.exports.MemComponent = require('./lib/components/mem-component');
+module.exports.GlobalLockComponent = require('./lib/components/globallock-component');
+module.exports.OpCountersComponent = require('./lib/components/opcounters-component');
--- test/electron/renderer/index.js
+++ test/electron/renderer/index.js
@@ -7,24 +7,42 @@ const ReactDOM = require('react-dom');
 
 // const CurrentOpComponent = require('../../../index').CurrentOpComponent;
 // const CurrentOpStore = require('./current-op-store');
-
-const OpCountersComponent = require('../../../index').OpCountersComponent;
-
 // const TopComponent = require('../../../index').TopComponent;
 // const TopStore = require('./top-store');
 
+const OpCountersComponent = require('../../../index').OpCountersComponent;
+const NetworkComponent = require('../../../index').NetworkComponent;
+const MemComponent = require('../../../index').MemComponent;
+const GlobalLockComponent = require('../../../index').GlobalLockComponent;
+
+
 const OpCountersStore = require('./opcounters-store');
+const NetworkStore = require('./network-store');
+const MemStore = require('./mem-store');
+const GlobalLockStore = require('./globallock-store');
 
 
 // ReactDOM.render(
 //   React.createElement(CurrentOpComponent, { store: CurrentOpStore, interval: 2000 }),
 //   document.getElementById('currentOpContainer') // eslint-disable-line no-undef
 // );
-ReactDOM.render(
-    React.createElement(OpCountersComponent, { store: OpCountersStore, interval: 1000 }),
-    document.getElementById('serverStatsContainer') // eslint-disable-line no-undef
-);
 // ReactDOM.render(
 //     React.createElement(TopComponent, { store: TopStore, interval: 2000 }),
 //     document.getElementById('topContainer') // eslint-disable-line no-undef
 // );
+ReactDOM.render(
+    React.createElement(OpCountersComponent, { store: OpCountersStore, interval: 1000 }),
+    document.getElementById('opCounterContainer') // eslint-disable-line no-undef
+);
+ReactDOM.render(
+  React.createElement(NetworkComponent, { store: NetworkStore, interval: 1000 }),
+  document.getElementById('networkContainer') // eslint-disable-line no-undef
+);
+ReactDOM.render(
+  React.createElement(MemComponent, { store: MemStore, interval: 1000 }),
+  document.getElementById('memContainer') // eslint-disable-line no-undef
+);
+ReactDOM.render(
+  React.createElement(GlobalLockComponent, { store: GlobalLockStore, interval: 1000 }),
+  document.getElementById('globalLockContainer') // eslint-disable-line no-undef
+);
