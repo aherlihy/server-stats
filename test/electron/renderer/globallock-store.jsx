cc9eb7f29fc30c310099d9594064a2c1c428261a
aherlihy <anna.herlihy@10gen.com>
2016-08-24 20:52:09 +0200

    changed read/write graph to take raw values

--- test/electron/renderer/globallock-store.jsx
+++ test/electron/renderer/globallock-store.jsx
@@ -9,20 +9,19 @@ const GlobalLockStore = Reflux.createStore({
   init: function() {
     this.listenTo(ServerStatsStore, this.globalLock);
 
-    this.opsPerSec = {'aReads': [], 'aWrites': [], 'qReads': [], 'qWrites': []};
+    this.totalCount = {'aReads': [], 'aWrites': [], 'qReads': [], 'qWrites': []};
     this.rawData = [];
     this.localTime = [];
     this.currentMax = 1;
-    this.currentMin = 0;
     this.starting = true;
     this.maxOps = 63;
     this.data = {'operations': [
-      {'op': 'aReads', 'count': [], 'active': true, 'current': 0},
-      {'op': 'aWrites', 'count': [], 'active': true, 'current': 0},
-      {'op': 'qReads', 'count': [], 'active': true, 'current': 0},
-      {'op': 'qWrites', 'count': [], 'active': true, 'current': 0}],
+      {'op': 'aReads', 'count': [], 'active': true},
+      {'op': 'aWrites', 'count': [], 'active': true},
+      {'op': 'qReads', 'count': [], 'active': true},
+      {'op': 'qWrites', 'count': [], 'active': true}],
       'localTime': [],
-      'yDomain': [this.currentMin, this.currentMax],
+      'yDomain': [0, this.currentMax],
       'rawData': [],
       'maxOps': this.maxOps,
       'labels': {
@@ -37,7 +36,6 @@ const GlobalLockStore = Reflux.createStore({
       if (!error && doc) {
         var key;
         var val;
-        var count;
         var raw = {};
         raw['aReads'] = doc.globalLock.activeClients.readers;
         raw['aWrites'] = doc.globalLock.activeClients.writers;
@@ -45,27 +43,22 @@ const GlobalLockStore = Reflux.createStore({
         raw['qWrites'] = doc.globalLock.currentQueue.writers;
         for (var q = 0; q < this.data.operations.length; q++) {
           key = this.data.operations[q].op;
-          count = raw[key];
-          if (this.starting) { // don't add data, starting point
-            this.data.operations[q].current = count;
+          val = raw[key];
+          if (this.starting) { //TODO: should we skip the first value to be consistent with the other graphs?
             continue;
           }
-          val = count - this.data.operations[q].current;
-          this.opsPerSec[key].push(val);
-          this.data.operations[q].count = this.opsPerSec[key].slice(Math.max(this.opsPerSec[key].length - this.maxOps, 0));
+          this.totalCount[key].push(val);
+          this.data.operations[q].count = this.totalCount[key].slice(Math.max(this.totalCount[key].length - this.maxOps, 0));
           if (val > this.currentMax) {
             this.currentMax = val;
-          } else if(val < this.currentMin) {
-            this.currentMin = val;
           }
-          this.data.operations[q].current = count;
         }
         if (this.starting) {
           this.starting = false;
           return;
         }
         this.rawData.push(raw);
-        this.data.yDomain = [this.currentMin, this.currentMax];
+        this.data.yDomain = [0, this.currentMax];
         this.localTime.push(doc.localTime);
         this.data.localTime = this.localTime.slice(Math.max(this.localTime.length - this.maxOps, 0));
         this.data.rawData = this.rawData.slice(Math.max(this.rawData.length - this.maxOps, 0));
